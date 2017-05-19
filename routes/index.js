b8dd833060d3944e3829f2c0fcfa48133800973c
anna <1018915057@qq.com>
2017-05-19 15:26:08 +0800

    add other pages

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,26 @@ router.get('/bg', function(req, res, next) {
 router.get('/tz', function(req, res, next) {
   res.render('tz', { title: '创建图纸' });
 });
+router.get('/case', function(req, res, next) {
+  res.render('case', { title: '我的项目' });
+});
+router.get('/fileUpdate', function(req, res, next) {
+  res.render('fileUpdate', { title: '文件更新提醒' });
+});
+router.get('/proofread', function(req, res, next) {
+  res.render('proofread', { title: '校审' });
+});
+router.get('/tizi', function(req, res, next) {
+  res.render('tizi', { title: '提资' });
+});
+router.get('/zuku', function(req, res, next) {
+  res.render('zuku', { title: '族库' });
+});
+router.get('/dianqigui', function(req, res, next) {
+  res.render('dianqigui', { title: '插入电气柜' });
+});
+
+
+
+
 module.exports = router;
